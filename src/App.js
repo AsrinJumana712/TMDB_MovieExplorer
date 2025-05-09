a792cdfb2e21f2f88b07c69840a9698b0018066f
A.R.Asrin Jumana <145181474+AsrinJumana712@users.noreply.github.com>
2025-05-09 23:51:08 +0530

    add movie details components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import MoviePage from "./Pages/MoviePage";
+import { ContextProvider } from "./Context/Context";
+import MovieDetails from "./Components/MovieDetails";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/movie" element={<MoviePage />} />
-        <Route path="*" element={<h1>404 - Page not found.</h1>} />
-      </Routes>
-    </Router>
+    <ContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/movie" element={<MoviePage />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="*" element={<h1>404 - Page not found.</h1>} />
+        </Routes>
+      </Router>
+    </ContextProvider>
   );
 }
 
