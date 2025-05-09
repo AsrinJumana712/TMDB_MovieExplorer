25006fb121179a19d75f82207e2f8b004441c33b
A.R.Asrin Jumana <145181474+AsrinJumana712@users.noreply.github.com>
2025-05-09 21:06:22 +0530

    Set up basic routing with React Router for Home, Login, and Movie pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import MoviePage from "./Pages/MoviePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/movie" element={<MoviePage />} />
+        <Route path="*" element={<h1>404 - Page not found.</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
